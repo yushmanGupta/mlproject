9a57693ce8a413a3eb921b9c05b4af5c39d70108
yushmanGupta <ayu1437mannu@gmail.com>
2025-04-10 12:34:51 +0530

    dt done

--- src/exception.py
+++ src/exception.py
@@ -1,12 +1,16 @@
 import sys
+#from src.logger 
+import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-    file_name,exc_tb.tb_lineno,str(error)
-    )
+     file_name,exc_tb.tb_lineno,str(error))
+
     return error_message
+
+    
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
@@ -15,3 +19,7 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+    
+
+
+        
--- src/logger.py
+++ src/logger.py
@@ -1,12 +1,12 @@
-import logging 
-import os 
+import logging
+import os
 from datetime import datetime
 
-LOG_FIILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-log_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exit_ok=True)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-LOG_FILE_PATH=od,path.join(logs_path,LOG_FIILE)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
@@ -15,3 +15,6 @@ logging.basicConfig(
 
 
 )
+
+if __name__=="__main__":
+    logging.info("logging has started")
